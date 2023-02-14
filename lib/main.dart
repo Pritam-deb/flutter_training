41c3d6d8eff83590ff34330a65f404a083ac8f8b
Pritam-debnath <pritamd@geekyants.com>
2023-02-14 11:25:32 +0530

    learnt: useState

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: 'Project App',
     theme: ThemeData(
       useMaterial3: true,
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
     ),
     home: MyHomePage(),
-    routes: {
-      '/new-contact': (context) => const NewContactView(),
-    },
   ));
 }
 
-class Contact {
-  final String id;
-  final String name;
-  Contact({
-    required this.name,
-  }) : id = const Uuid().v1();
-}
-
-class ContactBook extends ValueNotifier<List<Contact>> {
-  ContactBook._sharedInstance() : super([]);
-  static final ContactBook _shared = ContactBook._sharedInstance();
-  factory ContactBook() => _shared;
-  int get length => value.length;
-  void add({required Contact contact}) {
-    final contacts = value;
-    contacts.add(contact);
-    notifyListeners();
-  }
-
-  void remove({required Contact contact}) {
-    final contacts = value;
-    if (contacts.contains(contact)) {
-      value.remove(contact);
-      notifyListeners();
-    }
-  }
-
-  Contact? contact({required int atIndex}) =>
-      value.length > atIndex ? value[atIndex] : null;
-}
-
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    final contactBook = ContactBook();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: const Text('Home Page'),
-      ),
-      body: ValueListenableBuilder(
-        valueListenable: ContactBook(),
-        builder: (contact, value, child) {
-          final contacts = value as List<Contact>;
-          return ListView.builder(
-            itemCount: contacts.length,
-            itemBuilder: (context, index) {
-              final contact = contacts[index];
-              return Dismissible(
-                onDismissed: (direction) {
-                  ContactBook().remove(contact: contact);
-                },
-                key: ValueKey(contact.id),
-                child: Material(
-                  elevation: 0.6,
-                  color: Colors.white,
-                  child: ListTile(
-                    title: Text(contact.name),
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          Navigator.of(context).pushNamed('/new-contact');
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class NewContactView extends StatefulWidget {
-  const NewContactView({super.key});
-
-  @override
-  State<NewContactView> createState() => _NewContactViewState();
-}
-
-class _NewContactViewState extends State<NewContactView> {
-  late final TextEditingController _controller;
-  @override
-  void initState() {
-    _controller = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
+  String title = 'Tap the screen';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add new contact.'),
+        title: Text(title),
       ),
-      body: Column(children: [
-        TextField(
-          controller: _controller,
-          decoration:
-              InputDecoration(hintText: 'Enter a new contact name here'),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        },
+        child: Container(
+          color: Colors.white,
         ),
-        TextButton(
-            onPressed: () {
-              final contact = Contact(name: _controller.text);
-              ContactBook().add(contact: contact);
-              Navigator.of(context).pop();
-            },
-            child: Text('Add to contact list'))
-      ]),
+      ),
     );
   }
 }
